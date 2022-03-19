d04165f633369f51d0cf6696babc82416020bfba
Sebastian Heath <sebastian.heath@gmail.com>
2022-03-18 23:22:32 -0400

    test json

--- main.py
+++ main.py
@@ -692,7 +692,7 @@ def test_image_gallery(identifier):
   r = plodlib.PLODResource(identifier)
 
   with html_dom:
-    span(r.images)
+    span(identifier)
   return html_dom.render()
 
   r_images = json.loads(r.images)
